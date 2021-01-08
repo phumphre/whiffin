7b86cee9e6a8eeb0d7516cbb949128da5febe670
Parris Humphrey <phumphre@myriad.com>
2021-01-08 15:52:15 -0700

    added tooltip

--- js/whiffin.js
+++ js/whiffin.js
@@ -1,6 +1,5 @@
-
 function generate_dataset(het, prev) {
-    pen = d3.range(0.05, 1.05, 0.05).map(d3.format("0.2"))
+    pen = d3.range(0.05, 1.05, 0.005).map(d3.format("0.2"))
     var maf = whiffin(het, prev, pen);
     var dataset = [];
     for (i = 0; i < maf.length; i++) {
@@ -44,18 +43,23 @@ class WhiffinPlot {
         //Init dataset
         this.het = 0.05;
         this.prev = 1000;
-        this.data_init = generate_dataset(this.het, this.prev);
+        var dataset = generate_dataset(this.het, this.prev);
         //console.log("this.data_init:", this.data_init);
 
         //Init plot with axes
         //console.log("init:", this.data_init);
         var xScale = d3.scaleLinear()
-            .domain([0, d3.max(this.data_init, d => +d.x)]).nice()
+            .domain([0, d3.max(dataset, d => +d.x)]).nice()
             .range([0, this.width]);
 
         var yScale = d3.scaleLinear()
-            .domain([0, d3.max(this.data_init, d => +d.y)]).nice()
+            .domain([0, d3.max(dataset, d => +d.y)]).nice()
             .range([this.height - 150, 0]);
+
+        var tooltip = d3.select("body")
+            .append("div")   
+                .attr("class", "tooltip")
+                .style("opacity", 0);
 
         this.svg
             .append("g")
@@ -76,6 +80,7 @@ class WhiffinPlot {
             .style("text-anchor", "middle")
             .style("font-size", "12px")
             .text("Penetrance");
+
         this.svg
             .append("text")
             .attr("transform", "rotate(-90)")
@@ -85,6 +90,7 @@ class WhiffinPlot {
             .style("text-anchor", "middle")
             .style("font-size", "12px")
             .text("Max. credible allele freq.");
+
         //Init curve with defaults:
         this.line = d3.line()
             .x(function (d) { return xScale(d.x); })
@@ -94,12 +100,83 @@ class WhiffinPlot {
         this.myLine = this.svg
             .append("g")
                 .append("path")
-                .datum(this.data_init)
+                .datum(dataset)
                 .attr("d", this.line)
                     .attr("fill", "none")
                     .attr("stroke", "purple")
                     .attr("opacity", "0.8")
                     .attr("stroke-width", 1.5);
+
+        //Tooltip code
+        var bisect = d3.bisector(function(d) { return d.x; }).left;
+
+        // Create the circle that travels along the curve of chart
+        var focus = this.svg
+            .append('g')
+                .append('circle')
+                    .style("fill", "darkgray")
+                    .attr("stroke", "black")
+                    .attr('r', 5)
+                    .style("opacity", 0)
+
+        // Create the text that travels along the curve of chart
+        var focusText = this.svg
+            .append('g')
+                .append('text')
+                    .style("opacity", 0)
+                    .attr("text-anchor", "left")
+                    .attr("alignment-baseline", "middle")
+                    .style("font-size", "9px")
+
+        var focusText2 = this.svg
+            .append('g')
+                .append('text')
+                    .style("opacity", 0)
+                    .attr("text-anchor", "left")
+                    .attr("alignment-baseline", "middle")
+                    .style("font-size", "9px")
+
+        // Create a rect on top of the svg area: this rectangle recovers mouse position
+        this.svg
+          .append('rect')
+            .style("fill", "none")
+            .style("pointer-events", "all")
+            .attr('width', this.width)
+            .attr('height', this.height)
+            .on('mouseover', mouseover)
+            .on('mousemove', mousemove)
+            .on('mouseout', mouseout);
+
+        // What happens when the mouse move -> show the annotations at the right positions.
+        function mouseover() {
+            focus.style("opacity", 0.7)
+            focusText.style("opacity",1)
+            focusText2.style("opacity",1)
+        }
+
+        function mousemove() {
+            // recover coordinate we need
+            var x0 = xScale.invert(d3.mouse(this)[0]);
+            var i = bisect(dataset, x0, 1);
+            var selectedData = dataset[i];
+            focus
+                .attr("cx", xScale(selectedData.x))
+                .attr("cy", yScale(selectedData.y))
+            focusText
+                .html("Penetrance: " + selectedData.x)
+                    .attr("x", xScale(selectedData.x) + 15)
+                    .attr("y", yScale(selectedData.y) - 12)
+            focusText2
+                .html("MCAF: " + selectedData.y)
+                    .attr("x", xScale(selectedData.x) + 15)
+                    .attr("y", yScale(selectedData.y) - 3)
+        }
+
+        function mouseout() {
+            focus.style("opacity", 0)
+            focusText.style("opacity", 0)
+            focusText2.style("opacity", 0)
+        }
     };
 
     update(dataset) {
